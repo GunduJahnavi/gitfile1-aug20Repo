0dc73ace7884ada40ed01c613f98c9797d43633c
gundujahnavi <jahnavigundu01@gmail.com>
2024-08-28 19:12:33 +0530

    feature202 changes by Ethan

--- Application.java
+++ Application.java
@@ -7,7 +7,16 @@ public class Simple Method Example
         int sum = a + b;
         return sum;
     }
+#feature202 changes on reports module by Ethan in sept 2024
+// Public method to access private members
+    public void accessPrivateMembers() {
+        // Accessing the private field
+        System.out.println("Private Field: " + privateField);
 
+        // Calling the private method
+        privateMethod();
+    }
+#end of feature202 changes
      public static void main(String[] args) 
     {
         // Calling the method and storing the result
