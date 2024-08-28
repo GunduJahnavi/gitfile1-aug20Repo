136bf8fe733f70c058211e86434bf5a12db77651
gundujahnavi <jahnavigundu01@gmail.com>
2024-08-28 18:58:00 +0530

    feature201 changes in Application.java file by vivek

--- Application.java
+++ Application.java
@@ -7,6 +7,18 @@ public class Simple Method Example
         int sum = a + b;
         return sum;
     }
+#feature201 changes by Developer vivek for the module paymentgateway sept 2024
+// Main method to run the program
+    public static void main(String[] args) {
+        // Creating an object of MyClass
+        MyClass myObject = new MyClass();
+
+        // Accessing the public field
+        System.out.println("Public Field: " + myObject.publicField);
+
+        // Calling the public method
+        myObject.publicMethod();
+#end of feature201 code
 
      public static void main(String[] args) 
     {
