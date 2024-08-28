da5664a2364b92d5687af5e8cd467f8ab107c7d3
gundujahnavi <jahnavigundu01@gmail.com>
2024-08-28 18:42:01 +0530

    Application.java file added

---
+++ Application.java
@@ -0,0 +1,19 @@
+public class Simple Method Example
+{
+
+    // Method that takes two integers and returns their sum
+    public static int add Numbers(int a, int b)
+    {
+        int sum = a + b;
+        return sum;
+    }
+
+     public static void main(String[] args) 
+    {
+        // Calling the method and storing the result
+        int result = add Numbers(5, 7);
+
+        // Printing the result
+        System.out.println ("The sum is: " + result);
+    }
+}
